b282cce1fcd559bac8fa4aa21c66cb6ed8fd6151
Vladimir-od <58466406+Vladimir-od@users.noreply.github.com>
2020-12-11 10:35:40 +0200

    Add files via upload

--- test.mjs
+++ test.mjs
@@ -1,33 +1,20 @@
 "use strict";
-import cheerio from 'cheerio';
 import express from 'express';
 let app = express();
-import path from 'path';
 import req from 'request';
-const __dirname = path.resolve();
-
-
+app.set("view engine", "hbs");
 app.use(function (request, response) {
-   req('https://reqres.in/api/users?page=2',(err,res,body)=>{
-    let $ = cheerio.load(body);
-    let html = $.html()
-        
-   
-   
-      
-//   let html =  $(body).map(user=>{
-//         return `<div class="user"
-//         <p><img src="${user.avatar}"alt=""
-//         <p>${user.first_name}</p>
-//         <p>${user.email}</p>
-//         </div>`
-//     }).join('');
-//     console.log(html)
-    
-    // response.sendFile(__dirname + '/test.html')
-    response.send(html)
-   })
-   
+    req('https://reqres.in/api/users?page=2', (err, res, body) => {
+        let contacts = JSON.parse(body);
+        const html = contacts.data.map(user => {
+           return `<div class="user">
+        <p><img src="${user.avatar}"alt=""</p>
+        <p>${user.first_name}</p>
+        <p>${user.email}</p>
+        </div>`
+        }).join('');
+        response.send(html)
+    })
 })
 app.listen(3000, console.log(`server is listening on 3000`));
 
@@ -38,23 +25,3 @@ app.listen(3000, console.log(`server is listening on 3000`));
 
 
 
-
-
-// import request from 'request';
-// let data = document.getElementById("#app");
-// app.get("/", (req, response) => {
-
-//     request('https://reqres.in/api/users?page=2', (error, res, body) => {
-//         if (!error && res.statusCode == 200) {
-
-//         //    data.innerHTML = body;
-//             response.sendFile(__dirname + "/test.html")
-//         }
-//     })
-// })
-// app.listen(3000, (err) => {
-//     if (err) {
-//         return console.log('something bad happened', err)
-//     }
-//     console.log(`server is listening on 3000`)
-// });
