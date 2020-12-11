8bd110bd70ffbd4852a237af5aab3a698ed0593c
Vladimir-od <58466406+Vladimir-od@users.noreply.github.com>
2020-12-11 09:09:26 +0200

    Add files via upload

--- test.mjs
+++ test.mjs
@@ -1,13 +1,33 @@
 "use strict";
+import cheerio from 'cheerio';
 import express from 'express';
 let app = express();
 import path from 'path';
+import req from 'request';
 const __dirname = path.resolve();
 
 
 app.use(function (request, response) {
+   req('https://reqres.in/api/users?page=2',(err,res,body)=>{
+    let $ = cheerio.load(body);
+    let html = $.html()
+        
    
-    response.sendFile(__dirname + '/test.html')
+   
+      
+//   let html =  $(body).map(user=>{
+//         return `<div class="user"
+//         <p><img src="${user.avatar}"alt=""
+//         <p>${user.first_name}</p>
+//         <p>${user.email}</p>
+//         </div>`
+//     }).join('');
+//     console.log(html)
+    
+    // response.sendFile(__dirname + '/test.html')
+    response.send(html)
+   })
+   
 })
 app.listen(3000, console.log(`server is listening on 3000`));
 
