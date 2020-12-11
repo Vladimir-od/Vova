e6f6db13978db3feb4762abce9175c5e38c8a6ad
Roman Skaletskiy <roman.skaletskiy@onsolve.com>
2020-12-11 18:30:09 +0200

    public folder & hbs render

--- test.mjs
+++ test.mjs
@@ -1,27 +1,28 @@
 "use strict";
-import express from 'express';
+import express from "express";
+import req from "request";
+
 let app = express();
-import req from 'request';
+
+app.use(express.static("public"));
+
 app.set("view engine", "hbs");
+
 app.use(function (request, response) {
-    req('https://reqres.in/api/users?page=2', (err, res, body) => {
-        let contacts = JSON.parse(body);
-        const html = contacts.data.map(user => {
-           return `<div class="user">
-        <p><img src="${user.avatar}"alt=""</p>
-        <p>${user.first_name}</p>
-        <p>${user.email}</p>
-        </div>`
-        }).join('');
-        response.send(html)
-    })
-})
+  req("https://reqres.in/api/users?page=2", (err, res, body) => {
+    let contacts = JSON.parse(body);
+    const html = contacts.data
+      .map((user) => {
+        return `<div class="user">
+      <p><img src="${user.avatar}"alt=""</p>
+      <p>${user.first_name}</p>
+      <p>${user.email}</p>
+      </div>`;
+      })
+      .join("");
+
+    response.render("contact.hbs", { content: html });
+  });
+});
+
 app.listen(3000, console.log(`server is listening on 3000`));
-
-
-
-
-
-
-
-
